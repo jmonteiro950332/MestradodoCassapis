67a1a6b894b2f52ffbad4a2d3448dc7315f8fb1f
jmonteiro950332 <joao.t.s.monteiro@gmail.com>
2026-08-28 17:07:11 +0100

    Add files via upload

---
+++ Dados.R
@@ -0,0 +1,6 @@
+ados<-c("Baby Boomer","Millennial","Alpha")
+baby<-c(2/3,1/3,1/3)
+millenial<-c(3/3,2/3,3/3)
+alpha<-c(3/3,2/3,2/3)
+dados_geracoes<-data.frame(ados,baby,millenial,alpha)
+dados_geracoes
