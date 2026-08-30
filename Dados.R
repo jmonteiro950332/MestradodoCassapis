24d0feb3fa486c0f87f4f7d6685cbe7e7faa79f8
jmonteiro950332 <joao.t.s.monteiro@gmail.com>
2026-08-30 15:52:59 +0100

    Refactor data frame by removing 'ados' variable
    Removed 'ados' variable and updated data frame creation.

--- Dados.R
+++ Dados.R
@@ -1,8 +1,7 @@
-ados<-c("Baby Boomer","Millennial","Alpha")
 baby<-c(2/3,1/3,1/3)
 millenial<-c(3/3,2/3,3/3)
 alpha<-c(3/3,2/3,2/3)
-dados_geracoes<-data.frame(ados,baby,millenial,alpha)
+dados_geracoes<-data.frame(baby,millenial,alpha)
 dados_geracoes
 mean(c(0.3333333,1.0000000,0.6666667,0.3333333,0.6666667,0.6666667,0.6666667,1.0000000,1.0000000))
 median(c(0.3333333,1.0000000,0.6666667,0.3333333,0.6666667,0.6666667,0.6666667,1.0000000,1.0000000))
